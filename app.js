b6f97ca9f65066f0ec82908550de131315830166
Robert Nolan <robertnolan208@gmail.com>
2026-01-03 17:46:24 +0000

    Database Connected

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+
+//Connect to Database
+const mysql = require('mysql');
+
+//Create a connection to MySQL database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'product_data',
+});
+
+//Connect to database
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database', err);
+    } else {
+        console.log('Connected to database!');
+    }
+});
+
+//Start the server
 app.use(express.static("pages", {index: "home.html"}));
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server stated on port 3000');
+});
