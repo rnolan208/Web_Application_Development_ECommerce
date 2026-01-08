6a0386e197f9cc61fab053c2035fdd3f57207620
Robert Nolan <robertnolan208@gmail.com>
2026-01-08 16:18:11 +0000

    Updated mySQL database, still needs minor changes

--- database/db.js
+++ database/db.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'product_data',
+    database: 'proddata',
 });
 
 //Connect to database
