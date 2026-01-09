44a3dc538922e2cdcd0b8ef9acc81d868228fe6c
Robert Nolan <robertnolan208@gmail.com>
2026-01-09 21:46:12 +0000

    For loop in shopItems to select which image folder and whether to label item as  'League' or 'Product'. Updated shop pages for images and layout.

--- database/db.js
+++ database/db.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'proddata',
+    database: 'product_data',
 });
 
 //Connect to database
